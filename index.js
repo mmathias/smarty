b51329b13b09b9a1d5c00186a5b908d4e02d362d
Magno Mathias <mmathias@afilias.info>
2017-04-13 19:03:21 +0100

    update

--- index.js
+++ index.js
@@ -3,31 +3,22 @@ var hue = require("node-hue-api");
 var HueApi = hue.HueApi;
 var lightState = hue.lightState;
 var state = lightState.create();
+var axios = require('axios');
 
 var displayResults = function(result) {
     console.log(JSON.stringify(result, null, 2));
-};
-
-var displayResult = function(result) {
-    console.log(result);
 };
 
 var displayError = function(err) {
     console.error(err);
 };
 
-var turnOnLights = function() {
-
+var turnLights = function(lightsState) {
   var api = new HueApi('10.106.144.56', 'kdK77yMrADAVRAjQWixI1aMqn9qw4Y6TAKNgtqSI');
-  api.lights()
-      .then(displayResults);
 
-  api.setLightState(1, state.on(false)) // provide a value of false to turn off
-      .then(displayResult)
-      .fail(displayError)
-      .done();
+  console.log(lightsState);
 
-  api.setLightState(2, state.on(false)) // provide a value of false to turn off
+  return api.setLightState(3, state.on(lightsState)) // provide a value of false to turn off
       .then(displayResult)
       .fail(displayError)
       .done();
@@ -35,19 +26,26 @@ var turnOnLights = function() {
 
 
 EddystoneBeaconScanner.on('found', function(beacon) {
-  //console.log('found Eddystone Beacon:\n', JSON.stringify(beacon, null, 2));
+  // console.log('found Eddystone Beacon:\n', JSON.stringify(beacon, null, 2));
 
+  console.log('-----------------------------');
+  console.log(beacon.url === 'http://tiny.cc/mscgky' ? 'MAD ICE' : 'MAD BLUEBERRY');
+  console.log(beacon.url);
+  console.log(beacon.distance.toFixed(2));
 
-  turnOnLights();
+  if (beacon.distance < 0.5) {
+      axios.get(beacon.url)
+          .then(function(response) {
+                // console.log(response.data);
 
+                return response.data.lights === 'on';
+          })
+          .then(turnLights);
+  }
 });
 
 // EddystoneBeaconScanner.on('updated', function(beacon) {
 //   console.log('updated Eddystone Beacon:\n', JSON.stringify(beacon, null, 2));
-// });
-
-// EddystoneBeaconScanner.on('lost', function(beacon) {
-//   console.log('lost Eddystone beacon:\n', JSON.stringify(beacon, null, 2));
 // });
 
 EddystoneBeaconScanner.startScanning(true);
